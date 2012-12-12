7cc188dbe3f7ccf5a6b2758518bb23a548e476a1
Neel Hajare <neel.hajare@gmail.com>
2012-12-12 04:25:58 -0500

    Broken

---
+++ bootstrap.js
@@ -0,0 +1,77 @@
+/*
+ * This Source Code is subject to the terms of the Mozilla Public License
+ * version 2.0 (the "License"). You can obtain a copy of the License at
+ * http://mozilla.org/MPL/2.0/.
+ */
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cr = Components.results;
+const Cu = Components.utils;
+
+Cu.import("resource://gre/modules/Services.jsm");
+
+let addonData = null;
+
+function install(params, reason) {}
+function uninstall(params, reason) {}
+
+function startup(params, reason)
+{
+	addonData = params;
+
+	require("main");
+}
+
+function shutdown(params, reason)
+{
+	onShutdown.done = true;
+	for (let i = shutdownHandlers.length - 1; i >= 0; i --)
+	{
+		try
+		{
+			shutdownHandlers[i]();
+		}
+		catch (e)
+		{
+			Cu.reportError(e);
+		}
+	}
+}
+let shutdownHandlers = [];
+let onShutdown =
+{
+	done: false,
+	add: function(handler)
+	{
+		if (shutdownHandlers.indexOf(handler) < 0)
+			shutdownHandlers.push(handler);
+	},
+	remove: function(handler)
+	{
+		let index = shutdownHandlers.indexOf(handler);
+		if (index >= 0)
+			shutdownHandlers.splice(index, 1);
+	}
+};
+
+function require(module)
+{
+	let scopes = require.scopes;
+	if (!(module in scopes))
+	{
+			scopes[module] = {
+				Cc: Cc,
+				Ci: Ci,
+				Cr: Cr,
+				Cu: Cu,
+				require: require,
+				
+				onShutdown: onShutdown,
+				
+				exports: {}};
+			Services.scriptloader.loadSubScript(addonData.resourceURI.spec + module + ".js", scopes[module]);
+	}
+	return scopes[module].exports;
+}
+require.scopes = {__proto__: null};
---
+++ main.js
@@ -0,0 +1,92 @@
+/*
+ * This Source Code is subject to the terms of the Mozilla Public License
+ * version 2.0 (the "License"). You can obtain a copy of the License at
+ * http://mozilla.org/MPL/2.0/.
+ */
+
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+Cu.import("resource://gre/modules/Services.jsm");
+
+let policy =
+{
+	classDescription: "Test content policy",
+	classID: Components.ID("{12345678-1234-1234-1234-123456789abc}"),
+	contractID: "@adblockplus.org/test-policy;1",
+	xpcom_categories: ["content-policy"],
+
+	init: function()
+	{
+		let registrar = Components.manager.QueryInterface(Ci.nsIComponentRegistrar);
+		registrar.registerFactory(this.classID, this.classDescription, this.contractID, this);
+
+		let catMan = Cc["@mozilla.org/categorymanager;1"].getService(Ci.nsICategoryManager);
+		for each (let category in this.xpcom_categories)
+			catMan.addCategoryEntry(category, this.contractID, this.contractID, false, true);
+
+		onShutdown.add((function()
+		{
+			for each (let category in this.xpcom_categories)
+				catMan.deleteCategoryEntry(category, this.contractID, false);
+
+			// This needs to run asynchronously, see bug 753687
+			Services.tm.currentThread.dispatch(function()
+			{
+				registrar.unregisterFactory(this.classID, this);
+			}.bind(this), Ci.nsIEventTarget.DISPATCH_NORMAL);
+		}).bind(this));
+	},
+
+	// nsIContentPolicy interface implementation
+	shouldLoad: function(contentType, contentLocation, requestOrigin, node, mimeTypeGuess, extra)
+	{
+    var ww = Cc["@mozilla.org/embedcomp/window-watcher;1"].getService(
+        Components.interfaces.nsIWindowWatcher);
+
+    dump(ww.activeWindow.document.documentURI);
+    // var curIsHttps =  window.top.getBrowser().selectedBrowser.contentWindow.location.href.substr(0, 5) === 'https';
+
+    // dump(window.top.getBrowser().selectedBrowser.contentWindow.location.href);
+    // dump(curIsHttps);
+
+		// dump("shouldLoad: " + contentType + " " +
+		// 											(contentLocation ? contentLocation.spec : "null") + " " +
+		// 											(requestOrigin ? requestOrigin.spec : "null") + " " +
+		// 											node + " " +
+		// 											mimeTypeGuess + "\n");
+
+    // if (curIsHttps) {
+
+    //   if (contentType === Ci.nsIContentPolicy.TYPE_IMAGE ||
+    //       contentType === Ci.nsIContentPolicy.TYPE_STYLESHEET ||
+    //       contentType === Ci.nsIContentPolicy.TYPE_SCRIPT) {
+
+    //     dump(contentLocation.scheme); 
+    //   }
+    // }
+
+		return Ci.nsIContentPolicy.ACCEPT;
+	},
+
+	shouldProcess: function(contentType, contentLocation, requestOrigin, node, mimeTypeGuess, extra)
+	{
+		dump("shouldProcess: " + contentType + " " +
+														(contentLocation ? contentLocation.spec : "null") + " " +
+														(requestOrigin ? requestOrigin.spec : "null") + " " +
+														node + " " +
+														mimeTypeGuess + "\n");
+		return Ci.nsIContentPolicy.ACCEPT;
+	},
+
+	// nsIFactory interface implementation
+	createInstance: function(outer, iid)
+	{
+		if (outer)
+			throw Cr.NS_ERROR_NO_AGGREGATION;
+		return this.QueryInterface(iid);
+	},
+
+	// nsISupports interface implementation
+	QueryInterface: XPCOMUtils.generateQI([Ci.nsIContentPolicy, Ci.nsIFactory])
+};
+
+policy.init();
