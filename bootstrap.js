faedf950be88426440ea767aa9a6b1e6ec1f110c
Neel Hajare <neel.hajare@gmail.com>
2012-12-14 03:08:30 -0500

    Get rid of tabs

--- bootstrap.js
+++ bootstrap.js
@@ -18,60 +18,60 @@ function uninstall(params, reason) {}
 
 function startup(params, reason)
 {
-	addonData = params;
+  addonData = params;
 
-	require("main");
+  require("main");
 }
 
 function shutdown(params, reason)
 {
-	onShutdown.done = true;
-	for (let i = shutdownHandlers.length - 1; i >= 0; i --)
-	{
-		try
-		{
-			shutdownHandlers[i]();
-		}
-		catch (e)
-		{
-			Cu.reportError(e);
-		}
-	}
+  onShutdown.done = true;
+  for (let i = shutdownHandlers.length - 1; i >= 0; i --)
+  {
+    try
+    {
+      shutdownHandlers[i]();
+    }
+    catch (e)
+    {
+      Cu.reportError(e);
+    }
+  }
 }
 let shutdownHandlers = [];
 let onShutdown =
 {
-	done: false,
-	add: function(handler)
-	{
-		if (shutdownHandlers.indexOf(handler) < 0)
-			shutdownHandlers.push(handler);
-	},
-	remove: function(handler)
-	{
-		let index = shutdownHandlers.indexOf(handler);
-		if (index >= 0)
-			shutdownHandlers.splice(index, 1);
-	}
+  done: false,
+  add: function(handler)
+  {
+    if (shutdownHandlers.indexOf(handler) < 0)
+      shutdownHandlers.push(handler);
+  },
+  remove: function(handler)
+  {
+    let index = shutdownHandlers.indexOf(handler);
+    if (index >= 0)
+      shutdownHandlers.splice(index, 1);
+  }
 };
 
 function require(module)
 {
-	let scopes = require.scopes;
-	if (!(module in scopes))
-	{
-			scopes[module] = {
-				Cc: Cc,
-				Ci: Ci,
-				Cr: Cr,
-				Cu: Cu,
-				require: require,
-				
-				onShutdown: onShutdown,
-				
-				exports: {}};
-			Services.scriptloader.loadSubScript(addonData.resourceURI.spec + module + ".js", scopes[module]);
-	}
-	return scopes[module].exports;
+  let scopes = require.scopes;
+  if (!(module in scopes))
+  {
+      scopes[module] = {
+        Cc: Cc,
+        Ci: Ci,
+        Cr: Cr,
+        Cu: Cu,
+        require: require,
+        
+        onShutdown: onShutdown,
+        
+        exports: {}};
+      Services.scriptloader.loadSubScript(addonData.resourceURI.spec + module + ".js", scopes[module]);
+  }
+  return scopes[module].exports;
 }
 require.scopes = {__proto__: null};
--- main.js
+++ main.js
@@ -9,36 +9,36 @@ Cu.import("resource://gre/modules/Services.jsm");
 
 let policy =
 {
-	classDescription: "Test content policy",
-	classID: Components.ID("{12345678-1234-1234-1234-123456789abc}"),
-	contractID: "@adblockplus.org/test-policy;1",
-	xpcom_categories: ["content-policy"],
+  classDescription: "Test content policy",
+  classID: Components.ID("{12345678-1234-1234-1234-123456789abc}"),
+  contractID: "@adblockplus.org/test-policy;1",
+  xpcom_categories: ["content-policy"],
 
-	init: function()
-	{
-		let registrar = Components.manager.QueryInterface(Ci.nsIComponentRegistrar);
-		registrar.registerFactory(this.classID, this.classDescription, this.contractID, this);
+  init: function()
+  {
+    let registrar = Components.manager.QueryInterface(Ci.nsIComponentRegistrar);
+    registrar.registerFactory(this.classID, this.classDescription, this.contractID, this);
 
-		let catMan = Cc["@mozilla.org/categorymanager;1"].getService(Ci.nsICategoryManager);
-		for each (let category in this.xpcom_categories)
-			catMan.addCategoryEntry(category, this.contractID, this.contractID, false, true);
+    let catMan = Cc["@mozilla.org/categorymanager;1"].getService(Ci.nsICategoryManager);
+    for each (let category in this.xpcom_categories)
+      catMan.addCategoryEntry(category, this.contractID, this.contractID, false, true);
 
-		onShutdown.add((function()
-		{
-			for each (let category in this.xpcom_categories)
-				catMan.deleteCategoryEntry(category, this.contractID, false);
+    onShutdown.add((function()
+    {
+      for each (let category in this.xpcom_categories)
+        catMan.deleteCategoryEntry(category, this.contractID, false);
 
-			// This needs to run asynchronously, see bug 753687
-			Services.tm.currentThread.dispatch(function()
-			{
-				registrar.unregisterFactory(this.classID, this);
-			}.bind(this), Ci.nsIEventTarget.DISPATCH_NORMAL);
-		}).bind(this));
-	},
+      // This needs to run asynchronously, see bug 753687
+      Services.tm.currentThread.dispatch(function()
+      {
+        registrar.unregisterFactory(this.classID, this);
+      }.bind(this), Ci.nsIEventTarget.DISPATCH_NORMAL);
+    }).bind(this));
+  },
 
-	// nsIContentPolicy interface implementation
-	shouldLoad: function(contentType, contentLocation, requestOrigin, node, mimeTypeGuess, extra)
-	{
+  // nsIContentPolicy interface implementation
+  shouldLoad: function(contentType, contentLocation, requestOrigin, node, mimeTypeGuess, extra)
+  {
 
     // var curIsHttps =  window.top.getBrowser().selectedBrowser.contentWindow.location.href.substr(0, 5) === 'https';
 
@@ -47,11 +47,11 @@ let policy =
 
     // dump(contentLocation.scheme + " " + requestOrigin.scheme + "\n");
 
-		// dump("shouldLoad: " + contentType + " " +
-		// 											(contentLocation ? contentLocation.spec : "null") + " " +
-		// 											(requestOrigin ? requestOrigin.spec : "null") + " " +
-		// 											node + " " +
-		// 											mimeTypeGuess + "\n");
+    // dump("shouldLoad: " + contentType + " " +
+    //                       (contentLocation ? contentLocation.spec : "null") + " " +
+    //                       (requestOrigin ? requestOrigin.spec : "null") + " " +
+    //                       node + " " +
+    //                       mimeTypeGuess + "\n");
 
     if (requestOrigin.scheme === 'https' &&
         contentLocation.scheme !== 'https') {
@@ -64,29 +64,29 @@ let policy =
       }
     }
 
-		return Ci.nsIContentPolicy.ACCEPT;
-	},
+    return Ci.nsIContentPolicy.ACCEPT;
+  },
 
-	shouldProcess: function(contentType, contentLocation, requestOrigin, node, mimeTypeGuess, extra)
-	{
-		dump("shouldProcess: " + contentType + " " +
-														(contentLocation ? contentLocation.spec : "null") + " " +
-														(requestOrigin ? requestOrigin.spec : "null") + " " +
-														node + " " +
-														mimeTypeGuess + "\n");
-		return Ci.nsIContentPolicy.ACCEPT;
-	},
+  shouldProcess: function(contentType, contentLocation, requestOrigin, node, mimeTypeGuess, extra)
+  {
+    dump("shouldProcess: " + contentType + " " +
+                            (contentLocation ? contentLocation.spec : "null") + " " +
+                            (requestOrigin ? requestOrigin.spec : "null") + " " +
+                            node + " " +
+                            mimeTypeGuess + "\n");
+    return Ci.nsIContentPolicy.ACCEPT;
+  },
 
-	// nsIFactory interface implementation
-	createInstance: function(outer, iid)
-	{
-		if (outer)
-			throw Cr.NS_ERROR_NO_AGGREGATION;
-		return this.QueryInterface(iid);
-	},
+  // nsIFactory interface implementation
+  createInstance: function(outer, iid)
+  {
+    if (outer)
+      throw Cr.NS_ERROR_NO_AGGREGATION;
+    return this.QueryInterface(iid);
+  },
 
-	// nsISupports interface implementation
-	QueryInterface: XPCOMUtils.generateQI([Ci.nsIContentPolicy, Ci.nsIFactory])
+  // nsISupports interface implementation
+  QueryInterface: XPCOMUtils.generateQI([Ci.nsIContentPolicy, Ci.nsIFactory])
 };
 
 policy.init();
